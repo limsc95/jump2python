1f51dcc5abd0611427ecc57134e6aa71a430ef28
limsc <roko786@naver.com>
2026-05-29 13:17:43 +0900

    모듈

---
+++ 5/5-2.py
@@ -0,0 +1,13 @@
+# 모듈
+# 파일 자체
+# import mod1
+# print(mod1.add(4,2))
+
+# 특정 함수
+# from mod1 import add, sub
+# print(add(4,2))
+
+
+# 모든 함수
+from mod1 import *
+print(add(4,2))
---
+++ 5/mod1.py
@@ -0,0 +1,6 @@
+# 모듈
+def add(a,b):
+    return a+b
+
+def sub(a, b): 
+    return a-b
